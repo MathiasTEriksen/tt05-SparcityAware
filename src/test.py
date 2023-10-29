c10cbf877e827a34f498459a07aeccf6e0ea0622
Mathias Eriksen <mathias.torp.e@gmail.com>
2023-10-29 15:51:21 -0700

    changed to mvm

--- src/test.py
+++ src/test.py
@@ -3,5 +3,5 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_mvm(dut):
     dut._log.info("start")
