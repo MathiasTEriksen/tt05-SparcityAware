24a94d6a83e8402be586a54a04f7cfa576bd52d0
Mathias Eriksen <mathias.torp.e@gmail.com>
2023-10-30 15:43:21 -0700

    worked on code

--- src/mvm.v
+++ src/mvm.v
@@ -1,23 +1,36 @@
 module MVM_Accelerator (
-    input [3:0] spike_train,       // 4-input spike train
+    //input [3:0] spike_train,       // 4-input spike train
     input start,                   // Signal to start MVM
     input clk,                     // Clock
     input rst,                     // Reset
     input [1:0] row_val,            // CSR row pointers for 4x4 matrix
     input [7:0] value,              // CSR values for 4x4 matrix (assuming max 16 non-zero values)
-    input [1:0] column_val,   // CSR column indices for 4x4 matrix (assuming max 16 non-zero values)
-    output reg [7:0],       // Resultant output after MVM
-    output reg done
+    input [1:0] column_val,         // CSR column indices for 4x4 matrix (assuming max 16 non-zero values)
+    input sending_CPU,
+    input done_list,
+
+    output reg [7:0] output_val,               // Resultant output after MVM
+    output reg sending_out,
+    output reg FETCH_ready
 );
 
-parameter [1:0] IDLE    = 2'b00,
-                ADD   = 2'b01,
-                COMPUTE = 2'b10;
+reg [1:0] row_pointers[15:0];      // CSR row pointers for 4x4 matrix
+reg [7:0] values[15:0];           // CSR values for 4x4 matrix (assuming max 16 non-zero values)
+reg [1:0] column_indices[15:0];   // CSR column indices for 4x4 matrix (assuming max 16 non-zero values)
+reg [7:0] result[3:0];       // Resultant output after MVM
+reg [3:0] spike_train;       // 4-input spike train
+
+parameter [2:0] IDLE        = 3'b000,
+                TRANSMIT    = 3'b001,
+                COMPUTE     = 3'b010,
+                FETCH_CSR   = 3'b011,
+                FETCH_TRAIN = 3'b100;
 
 reg [1:0] state = IDLE;
 reg [1:0] current_row = 0;  // Current row being processed
 reg [3:0] i=0;
-
+reg [1:0] j=0;  
+  
 reg [7:0] interval;
 
 always @(posedge clk or posedge rst) begin
@@ -26,27 +39,63 @@ always @(posedge clk or posedge rst) begin
         current_row <= 0;
     end else begin
         case (state)
+
             IDLE: begin
                 if (start) begin
-                    state <= COMPUTE;
-                    done <= 0;
+                    state <= FETCH;
+                  
                 end
             end
 
-            COMPUTE: begin
+            FETCH_CSR: begin
                 
-                if (row_val == current_row) begin
-                    interval <= (spike_train[i]*value) + interval;
+                 FETCH_ready <= 1;
+                if (sending_CPU) begin
+                    FETCH_ready <= 0;
+                    row_pointers[i] <= row_val;
+                    column_indices[i] <= column_val;
+                    values[i] <= value;
+                    i <= i+1;                
+                end else if (done_list) begin
+                     FETCH_ready <= 0;
+                    state <= COMPUTE;
+                    i <= 0;
+                end   
+            end     
+
+            FETCH_TRAIN: begin
+                if (sending_CPU) begin
+                    spike_train <= value[3:0];
+                    FETCH_ready <= 1;
+                    state <= COMPUTE;
+                end            
+            end
+
+            COMPUTE: begin                
+                if (row_pointers[i] == current_row) begin
+                    interval <= ((spike_train[column_indices[i]])*(values[i])) + interval;
                     i <= i + 1;
                 end else if (current_row > 3) begin
-                    done <= 1;
-                    state <= IDLE;
+                    i <= 0;
+                    sending_out <= sending_out^0'b1;
+                    state <= TRANSMIT;
                 end else begin
                     result[current_row] <= interval;
                     interval <= 0;
                     current_row <= current_row + 1;
-                end                                               
+                end                                                 
             end
+
+            TRANSMIT: begin               
+              output_val <= result[j];
+                sending_out <= sending_out^0'b1;
+                j <= j+1;
+
+                if (i>3) begin
+                    state <= IDLE;  
+                    j <= 0; 
+               end 
+            end 
 
             default: state <= IDLE;
         endcase
--- src/tt_um_mvm.v
+++ src/tt_um_mvm.v
@@ -1,11 +1,11 @@
 `default_nettype none
 
 module tt_um_mvm #( parameter MAX_COUNT = 24'd10_000_000 ) (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] ui_in,    // Dedicated inputs - matrix values
+    output wire [7:0] uo_out,   // Dedicated outputs - output vector values
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path -> input, b1 sending, b3 ready for recieve, b45 col, b67 row 
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path -> b0 ready, b2 sending output
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output), 0b0000 0101
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -21,7 +21,7 @@ module tt_um_mvm #( parameter MAX_COUNT = 24'd10_000_000 ) (
     // assign uio_out = second_counter[7:0];
     
     // Instantiate lif neuron with next_state connected
-    MVM_Accelerator block1(.current(ui_in), .next_state(next_state), .spike(uio_out[0]), .clk(clk), .rst_n(rst_n));
+    MVM_Accelerator block1(.(ui_in), .next_state(next_state), .spike(uio_out[0]), .clk(clk), .rst_n(rst_n));
     
     // Output next_state to uo_out
     assign uo_out = next_state;
