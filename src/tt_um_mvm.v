fef9216d58849ee4cfa0b06a466ac4511dbbab7f
Mathias Eriksen <mathias.torp.e@gmail.com>
2023-10-30 23:51:56 -0700

    assigned uio_out and uio_in unused values

--- src/tt_um_mvm.v
+++ src/tt_um_mvm.v
@@ -16,7 +16,8 @@ module tt_um_mvm #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b00000011;
-    //assign uio_out [7:1] = 7'd0;
+    assign uio_out [7:2] = 6'd0;
+    assign uio_in [1:0] = 2'b0;
     // put bottom 8 bits of second counter out on the bidirectional gpio
     // assign uio_out = second_counter[7:0];
     
