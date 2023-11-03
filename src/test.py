995bc592ac6d42dca5d41c2be3fb82b9e2e3acf1
Mathias Eriksen <mathias.torp.e@gmail.com>
2023-11-03 16:14:49 -0700

    try indexing

--- src/test.py
+++ src/test.py
@@ -21,7 +21,7 @@ async def test_mvm(dut):
     dut.uio_in.value = 0b00100000
     await ClockCycles(dut.clk, 10)
 
-    temp = dut.uio_out.value & 0b10
+    temp = dut.uio_out.value[1]
 
     VALUES = [32,14,15,7]
     ROWS = [0,1,1,2]
@@ -70,14 +70,14 @@ async def test_mvm(dut):
     dut.ena.value = 0
     await ClockCycles(dut.clk, 1)
 
-    # while temp == (dut.uio_out.value & 0b10):
-    #     await ClockCycles(dut.clk, 1)
+    while temp == (dut.uio_out[1].value):
+        await ClockCycles(dut.clk, 1)
     
-    # await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 1)
     
-    # for j in range(3):
-    #     dut._log.info(dut.uo_out.value)
-    #     await ClockCycles(dut.clk, 1)
+    for j in range(3):
+        dut._log.info(dut.uo_out.value)
+        await ClockCycles(dut.clk, 1)
         
     for _ in range(100):    # runs for 100 clk cycles
         await RisingEdge(dut.clk)
